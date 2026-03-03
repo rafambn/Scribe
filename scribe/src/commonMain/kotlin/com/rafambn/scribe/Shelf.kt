24cbdb0a2acd0b23906ba46c7028d70ab06e7e9d
Rafael Mendonça <rafambn@gmail.com>
2026-03-03 16:24:42 -0300

    Refactor `NoteSaver`, `ScrollSaver`, and `RecordSaver` into functional interfaces
    - Convert `NoteSaver`, `ScrollSaver`, and `RecordSaver` from wrapper classes to `fun interface` definitions extending `Saver`.
    - Simplify the interface implementation by removing unnecessary delegation logic.

--- scribe/src/commonMain/kotlin/com/rafambn/scribe/Shelf.kt
+++ scribe/src/commonMain/kotlin/com/rafambn/scribe/Shelf.kt
@@ -4,6 +4,8 @@ fun interface Saver<T : Record> {
     suspend fun write(event: T)
 }
 
-class NoteSaver(val saver: Saver<Note>) : Saver<Note> by saver
-class ScrollSaver(val saver: Saver<SealedScroll>) : Saver<SealedScroll> by saver
-class RecordSaver(val saver: Saver<Record>) : Saver<Record> by saver
+fun interface NoteSaver : Saver<Note>
+
+fun interface ScrollSaver : Saver<SealedScroll>
+
+fun interface RecordSaver : Saver<Record>
